a8de124984c3ebe63e47cd6d1bbc58923bef4f59
Ben Schreiber <benschreiber93@gmail.com>
2014-11-25 15:24:42 -0500

    changed students view

--- sharples/views.py
+++ sharples/views.py
@@ -9,11 +9,16 @@ def index(request):
     
     
     currTime= datetime.datetime.now()
+    latestUpdateList= Update.objects.order_by('-when')
+    secondLatestUpdate=latestUpdateList[0]
     newUpdate = Update(when=currTime)
     newUpdate.save()
 
+    latest_interval_list = Interval.objects.order_by('-startTime')
+
     latest_update_list = Update.objects.order_by('-when')[:15] 
     # chose 5, but I was not sure how many updates I entered into the DB
+    #need to call computeInterval here
     template = loader.get_template('sharples/index.html')
     context = RequestContext(request,{'latest_update_list': latest_update_list,})
     return HttpResponse(template.render(context))
@@ -39,15 +44,49 @@ def computeInterval(paramSeconds):
             countedUpdates += 1
             if countedUpdates == 2:
                 intEndTime = update.when
-        else countedUpdates > 1: #if there are updates besides currUpdate
+        else if countedUpdates > 1: #if there are updates besides currUpdate
             currInterval = Interval(endTime = intEndTime, startTime = currTime, numUpdates = countedUpdates)
             currInterval.save()
             break
-    sortedIntervals = Interval.objects.order_by('-numUpdates') #descending order
+        """
+    
+def students(request):
+    print "called"
+    #latest_update_list = Update.objects.all()
+    updates = Update.objects.order_by('-when')
+    latest_update = updates[0]#Update.objects.get(id=len(latest_update_list))
+    print latest_update
+    
+    template = loader.get_template('students/indexStudents.html')
+    latest_update_list = Update.objects.order_by('-when')
+    percentileOfLatestInterval = computePercentile(latest_update_list, 500)
+    context = RequestContext(request,{'latest_update': latest_update, 'percentile':percentileOfLatestInterval,
+        
+    
+})
+    return HttpResponse(template.render(context))
+def computePercentile (sorted_update_list, paramSeconds):
+    countedUpdates = 1
+    sortedIntervals = Interval.objects.order_by('-numUpdates') #descending order    
+    currTime = datetime.datetime.now()
+    intEndTime = currTime #default endTime
+    for update in sorted_update_list:
+        #if update occured less than paramSeconds before currUpdate
+        updateCpy = update
+        naive = updateCpy.when.replace(tzinfo=None)
+        if (currTime - naive).seconds < paramSeconds :
+            countedUpdates += 1
+            if countedUpdates == 2:
+                intEndTime = update.when
+        elif countedUpdates >=1: #if there are updates besides currUpdate
+            currInterval = Interval(endTime = intEndTime, startTime = currTime, numUpdates = countedUpdates)
+            break
+
+        
     position = 0 #compare the interval with the most updates first
         
-    for interval in sortedIntervals:
-        if currInterval.numEntries < interval.numEntries:
+    for interval in sortedIntervals: #compute swipes percentile of currInterval
+        if currInterval.numUpdates < interval.numUpdates:
             position +=1
 
         else:
@@ -56,19 +95,6 @@ def computeInterval(paramSeconds):
     percentile = float(len(sortedIntervals)-position)/len(sortedIntervals)
     print percentile
     return percentile
-"""
-    
-def students(request):
-    print "called"
-    latest_update_list = Update.objects.all()
-    latest_update = Update.objects.get(id=len(latest_update_list))
-    print latest_update
-    template = loader.get_template('students/indexStudents.html')
-    latest_update_list = Update.objects.order_by('-when')
-    context = RequestContext(request,{'latest_update': latest_update,
-    
-})
-    return HttpResponse(template.render(context))
 
 def detail(request, update_id):
     return HttpResponse("You're looking at update %s." % update_id)
